430412b22af0892cdc68bc6ca8d01b650646d850
Drew Dahlquist <drewdahlquistdev@gmail.com>
2023-01-31 17:04:25 -0600

    basic endpoints w fake data for testing

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,20 @@
+require('dotenv').config()
+
 const express = require('express')
+const cors = require("cors");
+
 const app = express()
-const port = 3000
+const port = 5001;
+
+app.use(cors());
+app.use(express.json());
+app.use(require("./routes/user.js"));
+app.use(require("./routes/task.js"));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 })
